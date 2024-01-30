fcfe7455962ea82cbb74c65d58eea944548e3a5c
UDAY KUMAR <udaydevil2312@gmail.com>
2024-01-30 14:29:45 +0530

    adding new comment

---
+++ src/MailDetails.java
@@ -0,0 +1,5 @@
+public class MailDetails {
+    String Mail;
+    String Password;
+
+}
---
+++ src/MailProcess.java
@@ -0,0 +1,16 @@
+public class MailProcess {
+    boolean login(MailDetails details) {
+        String Mail="ram@gamil.com";
+        String Password="ram@123";
+        if (Mail.equals(details.Mail) && Password.equals(details.Password)) {
+            return true;
+
+
+        }else{
+            return false;
+        }
+
+
+    }
+
+}
